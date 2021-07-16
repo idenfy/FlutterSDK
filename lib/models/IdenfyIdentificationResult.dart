8b519fdbf342ac50fed61246ee839f7139c3cc5b
encikpulasan <azlan@programmer.net>
2021-07-16 14:08:39 +0800

    Update return null instead of ()  => null

--- lib/models/IdenfyIdentificationResult.dart
+++ lib/models/IdenfyIdentificationResult.dart
@@ -2,33 +2,28 @@ import 'AutoIdentificationStatus.dart';
 import 'ManualIdentificationStatus.dart';
 
 class IdenfyIdentificationResult {
-  final AutoIdentificationStatus? autoIdentificationStatus;
-  final ManualIdentificationStatus? manualIdentificationStatus;
+  final AutoIdentificationStatus autoIdentificationStatus;
+  final ManualIdentificationStatus manualIdentificationStatus;
 
-  IdenfyIdentificationResult(
-      this.autoIdentificationStatus, this.manualIdentificationStatus);
+  IdenfyIdentificationResult(this.autoIdentificationStatus, this.manualIdentificationStatus);
 
   factory IdenfyIdentificationResult.fromJson(dynamic json) {
     return IdenfyIdentificationResult(
-        AutoIdentificationStatus.values
-            .enumFromString(json['autoIdentificationStatus']),
-        ManualIdentificationStatus.values
-            .enumFromString(json['manualIdentificationStatus']));
+      AutoIdentificationStatus.values.enumFromString(json['autoIdentificationStatus']),
+      ManualIdentificationStatus.values.enumFromString(json['manualIdentificationStatus']),
+    );
   }
 }
 
 extension EnumTransform on List {
   String? string<T>(T value) {
     if (value == null || (isEmpty)) return null;
-    var occurence = singleWhere(
-        (enumItem) => enumItem.toString() == value.toString(),
-        orElse: () => null);
+    var occurence = singleWhere((enumItem) => enumItem.toString() == value.toString(), orElse: () => null);
     if (occurence == null) return null;
     return occurence.toString().split('.').last;
   }
 
-  T? enumFromString<T>(String? value) {
-    return firstWhere((type) => type.toString().split('.').last == value,
-        orElse: () => null);
+  T? enumFromString<T>(String value) {
+    return firstWhere((type) => type.toString().contains(value), orElse: null);
   }
 }
