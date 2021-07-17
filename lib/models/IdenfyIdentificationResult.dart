d90b5fbac6b0b18f805d6b820995c0bbec805e87
encikpulasan <azlan@programmer.net>
2021-07-17 08:46:34 +0800

    Update enumStringMethod

--- lib/models/IdenfyIdentificationResult.dart
+++ lib/models/IdenfyIdentificationResult.dart
@@ -18,12 +18,12 @@ class IdenfyIdentificationResult {
 extension EnumTransform on List {
   String? string<T>(T value) {
     if (value == null || (isEmpty)) return null;
-    var occurence = singleWhere((enumItem) => enumItem.toString() == value.toString(), orElse: () => null);
+    var occurence = singleWhere((enumItem) => enumItem.toString() == value.toString(), orElse: null);
     if (occurence == null) return null;
     return occurence.toString().split('.').last;
   }
 
   T? enumFromString<T>(String value) {
-    return firstWhere((type) => type.toString().contains(value), orElse: null);
+    return firstWhere((type) => type.toString().split('.').last == value, orElse: null);
   }
 }
